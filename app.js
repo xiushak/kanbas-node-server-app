8cfc722101cf8384768209aee1a3bda6758be379
xiushak <30637265+xiushak@users.noreply.github.com>
2023-11-13 15:26:00 -0500

    a5 update to use es6

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-// equivalent to import
-const express = require("express");
+import express from "express";
 // create new express instance
 const app = express();
-// create a route that responds 'hello'
 app.get("/hello", (req, res) => {
-  res.send("Hello World!");
+  res.send("Life is good!");
+});
+app.get("/", (req, res) => {
+  res.send("Welcome to Full Stack Development!");
 });
 // listen to http://localhost:4000
 app.listen(4000);
