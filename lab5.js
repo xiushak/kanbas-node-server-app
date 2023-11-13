076c31f30694c5ab11b183de6d137821984c20e5
xiushak <30637265+xiushak@users.noreply.github.com>
2023-11-13 16:01:41 -0500

    a5 3.3

--- lab5.js
+++ lab5.js
@@ -6,12 +6,55 @@ const assignment = {
   completed: false,
   score: 0,
 };
+const todos = [
+  { id: 1, title: "Task 1", completed: false },
+  { id: 2, title: "Task 2", completed: false },
+  { id: 3, title: "Task 3", completed: false },
+  { id: 4, title: "Task 4", completed: false },
+];
 
 const Lab5 = (app) => {
+  // array stuff
+  app.get("/a5/todos", (req, res) => {
+    const { completed } = req.query;
+    if (completed !== undefined) {
+      const completedTodos = todos.filter((t) => t.completed === completed);
+      res.json(completedTodos);
+      return;
+    }
+    res.json(todos);
+  });
+  app.get("/a5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    res.json(todo);
+  });
+  app.get("/a5/todos/:id/title/:title", (req, res) => {
+    const { id, title } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.title = title;
+    res.json(todos);
+  });
+  app.get("/a5/todos/create", (req, res) => {
+    const newTodo = {
+      id: new Date().getTime(),
+      title: "New Task",
+      completed: false,
+    };
+    todos.push(newTodo);
+    res.json(todos);
+  });
+  app.get("/a5/todos/:id/delete", (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todos.splice(todos.indexOf(todo), 1);
+    res.json(todos);
+  });
+
+  // assignment stuff
   app.get("/a5/assignment", (req, res) => {
     res.json(assignment);
   });
-
   app.get("/a5/assignment/title", (req, res) => {
     res.json(assignment.title);
   });
@@ -31,10 +74,10 @@ const Lab5 = (app) => {
     res.json(assignment);
   });
 
+  // calculator stuff
   app.get("/a5/welcome", (req, res) => {
     res.send("Welcome to Assignment 5");
   });
-
   app.get("/a5/add/:a/:b", (req, res) => {
     const { a, b } = req.params;
     const sum = parseInt(a) + parseInt(b);
@@ -45,7 +88,6 @@ const Lab5 = (app) => {
     const sum = parseInt(a) - parseInt(b);
     res.send(sum.toString());
   });
-
   app.get("/a5/calculator", (req, res) => {
     const { a, b, operation } = req.query;
     let result = 0;
